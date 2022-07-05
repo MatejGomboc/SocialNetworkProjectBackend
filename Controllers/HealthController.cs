947a85611f48ad6b906a851037347fbd7bbf9802
Matej Gomboc <matejg03@gmail.com>
2022-07-05 15:06:22 +0200

    Added health check.

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetworkProjectBackend.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class HealthController : ControllerBase
+    {
+        [HttpPost]
+        [Route("health")]
+        [AllowAnonymous]
+        public IActionResult Health()
+        {
+            return Ok("ok");
+        }
+    }
+}
